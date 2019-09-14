fa332c6ffdc28519a96963f625f2775e5382b2b9
孤城白云帝 <1017904699@qq.com>
2019-09-14 22:45:31 +0800

    完成攻略详情评论列表及分页展示以及其他攻略页面的功能优化

--- components/post/comment.vue
+++ components/post/comment.vue
@@ -2,13 +2,46 @@
   <div class="comment-main">
 
     <div class="comment-list">
-      <CommentList :data="commentList"></CommentList>
+      <div v-for="(item, index) in commentList" :key="index" class="comment-detail">
+        <div class="userInfo">
+          <img :src="`${$axios.defaults.baseURL}${item.account.defaultAvatar}`" class="user-img" alt="">
+          <span>{{ item.account.nickname }}</span>
+          <span>{{ item.updated_at | time }}</span>
+          <span class="com-rigth">{{ item.level }}</span>
+        </div>
+        <div class="comment-content">
+          <div class="user-floor">
+          <CommentList :data="item"></CommentList>
+          </div>
+          <div class="user-self">{{ item.content }}</div>
+          <el-row type="flex" :gutter="15">
+            <div class="comment-pic" v-for="(imgitem, index2) in item.pics" :key="index2">
+              <img :src="`${$axios.defaults.baseURL}${imgitem.url}`" alt="">
+            </div>
+          </el-row>
+          <div class="comment-ctrl" @mousemove="reply=true" @mouseout="reply=false">
+            <nuxt-link v-show="reply" to="#">回复</nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="block">
+      <el-pagination
+        :current-page="pageNum"
+        :page-sizes="[2, 3, 5, 10]"
+        :page-size="getCommentObj._limit"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import CommentList from '@/components/post/commentList'
+import moment from 'moment'
 export default {
   data(){
     return {
@@ -20,11 +53,20 @@ export default {
         // _sort: '',
         _limit: 2,
         _start: 0
-      }
+      },
+      pageNum: 1,
+      total: 0,
+      reply: false
     }
   },
   components:{
     CommentList
+  },
+  filters: {
+    time (value) {
+      const filtersTime = moment(value).format(`YYYY-MM-DD HH:MM`)
+      return filtersTime
+    }
   },
   methods:{
     // 获取评论方法
@@ -34,13 +76,32 @@ export default {
         params: this.getCommentObj
       })
       .then((res)=>{
+        console.log(res)
         this.commentList = res.data.data
         this.$emit('getTotal',res.data.total)
-        console.log(this.commentList)
+        this.total = res.data.total
       })
       .catch((err)=>{
         console.log(err)
       })
+    },
+    // 选择每页显示数据条数
+    handleSizeChange(val) {
+      // 每页显示条数赋值
+      this.getCommentObj._limit = val
+      // 开始数据从0开始
+      this.getCommentObj._start = 0
+      // 当前页码为1
+      this.pageNum = 1
+      this.init()
+    },
+    // 切换页码事件
+    handleCurrentChange(val) {
+      // 给当前页码赋值
+      this.pageNum = val
+      // 计算出数据从哪条开始
+      this.getCommentObj._start = (val - 1) * this.getCommentObj._limit
+      this.init()
     }
   },
   mounted(){
@@ -53,6 +114,49 @@ export default {
 .comment-main{
   .comment-list{
     border: 1px solid #ccc;
+    .comment-detail{
+      padding: 20px 20px 5px;
+      border-bottom: 1px dashed #ddd;
+      background: transparent;
+      .userInfo{
+        margin-bottom: 10px;
+        font-size: 12px;
+        color: #666;
+        .user-img{
+          width: 16px;
+          border-radius: 50%;
+          vertical-align: bottom;
+        }
+        .username{
+          color: orange;
+          margin-right: 5px;
+        }
+        .com-rigth{
+          float: right;
+        }
+      }
+      .user-self{
+        margin-top: 5px;
+      }
+      .comment-pic{
+        width: 80px;
+        height: 80px;
+        border-radius: 6px;
+        overflow: hidden;
+        margin-right: 5px;
+        margin-top: 10px;
+        padding: 5px;
+        border: 1px dashed #ddd;
+      }
+      .comment-ctrl{
+        height: 20px;
+        line-height: 20px;
+        font-size: 12px;
+        color: #1e50a2;
+        text-align: right;
+        cursor: pointer;
+      }
+    }
   }
 }
 </style>
--- components/post/commentList.vue
+++ components/post/commentList.vue
@@ -1,30 +1,107 @@
 <template>
   <div>
-    <div class="comment detail" v-for="(item, index) in data" :key="index">
-      <p>
-        <img :src="`${$axios.defaults.baseURL}${item.account.defaultAvatar}`" alt="">
-      </p>
+    <div class="comment-detail">
+      <div class="user-floor">
+        <CommentList :data="data.parent" v-if="data.parent"></CommentList>
+      </div>
+      <div class="comment-content">
+        <div class="userInfo">
+          <span>{{ data.account.nickname }}</span>
+          <span>{{ data.updated_at | time }}</span>
+          <span class="com-rigth">{{ data.level }}</span>
+        </div>
+        <div class="user-self">{{ data.content }}</div>
+        <el-row type="flex" :gutter="15">
+          <div class="comment-pic" v-for="(imgitem, index) in data.pics" :key="index">
+            <img :src="`${$axios.defaults.baseURL}${imgitem.url}`" alt="">
+          </div>
+        </el-row>
+        <div class="comment-ctrl" @mousemove="reply=true" @mouseout="reply=false">
+          <a v-show="reply===true" href="javascript:viod(0)">回复</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+import CommentList from '@/components/post/commentList'
 export default {
+  name: 'CommentList',
   props:{
     data:{
-      type: Array,
-      default: []
+      type: Object,
+      default: {}
+    }
+  },
+  data(){
+    return {
+      reply: false
+    }
+  },
+  components:{
+    CommentList
+  },
+  filters: {
+    time (value) {
+      const filtersTime = moment(value).format(`YYYY-MM-DD HH:MM`)
+      return filtersTime
     }
   },
   mounted(){
-    console.log(this.data)
   }
 }
 </script>
 
 <style lang="less" scoped>
   .comment-detail{
-    border-bottom: 1px dashed #ddd;
-    padding: 20px 20px 5px;
+    padding-left: 5px;
+    background: #f9f9f9;
+    border: 1px solid #ddd;
+    padding: 2px;
+    .comment-content{
+      padding-left: 10px;
+      padding-bottom: 5px; 
+      .userInfo{
+        margin-bottom: 5px;
+        padding-right: 10px;
+        font-size: 12px;
+        color: #666;
+        .user-img{
+          width: 16px;
+          border-radius: 50%;
+          vertical-align: bottom;
+        }
+        .username{
+          color: orange;
+          margin-right: 5px;
+        }
+        .com-rigth{
+          float: right;
+        }
+      }
+      .user-self{
+
+      }
+      .comment-pic{
+        width: 80px;
+        height: 80px;
+        border-radius: 6px;
+        overflow: hidden;
+        margin-right: 5px;
+        margin-top: 10px;
+        padding: 5px;
+        border: 1px dashed #ddd;
+      }
+      .comment-ctrl{
+        height: 20px;
+        line-height: 20px;
+        font-size: 12px;
+        color: #1e50a2;
+        text-align: right;
+        cursor: pointer;
+      }
+    }
   }
 </style>
